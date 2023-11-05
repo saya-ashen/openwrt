2d4817b470d3af05872a8896698676cd2a14c9e8
ashen <40572061+saya-ashen@users.noreply.github.com>
2023-11-05 14:00:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-package' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
